d0dc7f32ab5605fd4527ed3991c121e9d6a34d0c
sugurucode <tech.study409@gmail.com>
2024-09-12 12:17:06 +0900

    BASEURL

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
     //baseURL: "http://localhost:5000/api",
-    baseURL: process.env.NEXT_PIBLIC_API_BASEURL,
+    baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
     headers: {
         //"Content-Type": "application/json",
 
